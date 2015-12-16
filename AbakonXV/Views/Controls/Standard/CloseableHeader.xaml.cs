01203d2f31f4e198b38e5805d18afd344411c4b6
ZbyszekB <z.benedykt@post.pl>
2015-12-16 18:22:11 +0100

    Dłączono standardowe kontrolki

---
+++ AbakonXV/Views/Controls/Standard/CloseableHeader.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AbakonXVWPF.Views.Controls.Standard
+{
+    /// <summary>
+    /// Interaction logic for CloseableHeader.xaml
+    /// </summary>
+    public partial class CloseableHeader : UserControl
+    {
+        public CloseableHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AbakonXV/Views/Controls/Standard/FilterPanelUC.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using AbakonXVWPF.ViewModels;
+
+namespace AbakonXVWPF.Views.Controls.Standard
+{
+    /// <summary>
+    /// Interaction logic for FilterPanelUC.xaml
+    /// </summary>
+    public partial class FilterPanelUC : UserControl
+    {
+        public FilterPanelUC()
+        {
+            InitializeComponent();
+        }
+
+        private void _FieldsCollection_ListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ((FilterPanelVM)this.DataContext).AddToFieldsFiltered();
+        }
+
+        private void _FieldsFiltered_ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ((FilterPanelVM)this.DataContext).RemoveFromFieldsFiltered();
+        }
+
+        private void _FieldsCollection_ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (_FieldsCollection_ListView.SelectedItem != null)
+            {
+                _FieldsCollection_ListView.ScrollIntoView(_FieldsCollection_ListView.SelectedItem);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ((FilterPanelVM)this.DataContext).CurrentFromFieldsFiltered = (FilterField)((ListBoxItem)_FieldsFiltered_ListBox.ContainerFromElement((Button)sender)).Content;
+            ((FilterPanelVM)this.DataContext).RemoveFromFieldsFiltered();
+        }
+
+        private void _upButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((FilterPanelVM)this.DataContext).CurrentFromFieldsCollection = (FilterField)((ListBoxItem)_FieldsCollection_ListView.ContainerFromElement((Button)sender)).Content;
+            ((FilterPanelVM)this.DataContext).AddToFieldsFiltered();
+        }
+    }
+}
