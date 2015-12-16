b293689dd4eae09209c645ac1b718c6fa76fe8e4
ZbyszekB <z.benedykt@post.pl>
2015-12-16 18:19:05 +0100

    Dołączono style

---
+++ AbakonXV/Style/GeneralStyles.xalm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+
+namespace AbakonXVWPF.Styles
+{
+    partial class GeneralStyles
+    {
+        private void SelectColumn(object sender, RoutedEventArgs e)
+        {
+            var myDataGrid = FindVisualParent<DataGrid>((Button)sender);
+
+            if (myDataGrid.SelectionUnit == DataGridSelectionUnit.FullRow) myDataGrid.SelectionUnit = DataGridSelectionUnit.CellOrRowHeader;
+            {
+                DataGridColumnHeader header = FindVisualParent<DataGridColumnHeader>((Button)sender);
+                if (header != null)
+                {
+                    if (!((Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))))
+                    {
+                        myDataGrid.SelectedCells.Clear();
+                    }
+
+                    DataGridColumn column = header.Column;
+                    for (int i = 0; i < myDataGrid.Items.Count; i++)
+                    {
+                        DataGridCellInfo cellInfo = new DataGridCellInfo(myDataGrid.Items[i], column);
+                        if (!myDataGrid.SelectedCells.Contains(cellInfo))
+                        {
+                            myDataGrid.SelectedCells.Add(cellInfo);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static T FindVisualParent<T>(UIElement element) where T : UIElement
+        {
+            UIElement parent = element;
+            while (parent != null)
+            {
+                T correctlyTyped = parent as T;
+                if (correctlyTyped != null)
+                {
+                    return correctlyTyped;
+                }
+
+                parent = System.Windows.Media.VisualTreeHelper.GetParent(parent) as UIElement;
+            }
+
+            return null;
+        }
+
+
+    }
+}
